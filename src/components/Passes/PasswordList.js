79b043ecc32d94ebcd6d06b5ff25e6da573e6dd4
spidey52 <satyamkumar5254@gmail.com>
2021-02-04 13:14:19 +0530

    filtering added

--- src/components/Passes/PasswordList.js
+++ src/components/Passes/PasswordList.js
@@ -1,25 +1,49 @@
-import  { useContext } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import { PasswordContext } from "../../context/PasswordContext";
 import PasswordOne from "./PasswordOne";
 
 const PasswordList = () => {
   const { isLoading, passes } = useContext(PasswordContext);
 
+  const [data, setData] = useState([]);
+
+  const search = useRef();
+
+  const handleFilter = (e) => {
+    // FIXME: if filter is from api apply debounce here
+    setData(passes.filter((pass) => pass.name.includes(e.target.value)));
+  };
+
+  useEffect(() => {
+    search.current.focus();
+    setData(passes);
+  }, [passes]);
+
   return (
-    <div className="wrapper">
-      {isLoading
-        ? "loading"
-        : passes.map((e) => (
-            <PasswordOne
-              title={e.name}
-              username={e.username}
-              password={e.password}
-              email={e.email}
-              key={e._id}
-              id={e._id}
-            />
-          ))}
-    </div>
+    <>
+      <div className="search-container">
+        <input
+          type="text"
+          placeholder="search"
+          ref={search}
+          onChange={handleFilter}
+        />
+      </div>
+      <div className="wrapper">
+        {isLoading
+          ? "loading"
+          : data.map((e) => (
+              <PasswordOne
+                title={e.name}
+                username={e.username}
+                password={e.password}
+                email={e.email}
+                key={e._id}
+                id={e._id}
+              />
+            ))}
+      </div>
+    </>
   );
 };
 
