00c960db430ad1ffcb834446680b8887da7e9ede
spidey52 <satyamkumar5254@gmail.com>
2021-02-04 19:52:13 +0530

    done

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,10 +1,10 @@
 import React from "react";
-import loading from "../images/loading-arrow.gif";
+import loading from "../images/loading-arrow.svg";
 
 const Loading = () => {
   return (
-    <div style={{ width: "100%", paddingTop: "3rem", textAlign: "center" }}>
-      <img src={loading} alt="" />
+    <div style={{ width: "100%", paddingTop: "3rem", textAlign: "center"  }}>
+      <img width="100px" src={loading} className="rotate" alt="" />
     </div>
   );
 };
