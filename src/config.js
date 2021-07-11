2fb8c03195e62adc7ec5f28da651d490208d8add
satyam kumar <satyamkumar5254@gmail.com>
2021-07-11 12:08:35 +0530

    update api address

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const localApiAddress = "http://localhost:8400";
-const productionApiAddress = "https://spidey-passmanager.herokuapp.com";
+const productionApiAddress = "http://www.kaprabecho.ml";
 const apiAddress = process.env.NODE_ENV === 'development' ? localApiAddress : productionApiAddress
 
 
