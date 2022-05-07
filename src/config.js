dc1968b8c0828f43f964fb3ff51e2e162cfa382b
satyam kumar <satyamkumar5254@gmail.com>
2022-05-08 00:07:47 +0530

    update packages

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const localApiAddress = "http://localhost:5000";
 const productionApiAddress = "https://passmanager.fitsworld.online";
-const apiAddress = process.env.NODE_ENV === 'development' ? localApiAddress : productionApiAddress
+const apiAddress = process.env.NODE_ENV === 'development' ? productionApiAddress : productionApiAddress
 
 
 
